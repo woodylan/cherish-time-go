2ccd6c9c280c7e85f116585099a1c9a4371afe79
woodylan <woodylan@126.com>
2019-01-18 15:08:59 +0800

    添加redis模块

---
+++ cache/cache.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"github.com/astaxie/beego/cache"
+	"github.com/astaxie/beego"
+	"fmt"
+)
+
+var Bm cache.Cache
+
+func Init() {
+	var err error
+
+	key := beego.AppConfig.String("redis_key")
+	host := beego.AppConfig.String("redis_host")
+	port := beego.AppConfig.String("redis_port")
+	password := beego.AppConfig.String("redis_password")
+
+	//config:=`{"key":"cherishTime","conn":"127.0.0.1:6379","dbNum":"0","password":""}`
+	config := `{"key":"` + key + `","conn":"` + host + `:` + port + `","dbNum":"0","password":"` + password + `"}`
+	fmt.Println(config)
+
+	Bm, err = cache.NewCache("redis", config)
+	if err != nil {
+		beego.Debug("Redis init fail")
+	} else {
+		beego.Debug("Redis init success")
+	}
+}
